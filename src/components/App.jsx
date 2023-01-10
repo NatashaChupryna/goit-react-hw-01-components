1217b0cc9d2dda38b98972586017f68629883ba9
Natasha <natkamonte1992@gmail.com>
2023-01-10 13:57:00 +0200

    commented Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import Profile from './Profile/Profile';
+// import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
@@ -20,13 +20,13 @@ export const App = () => {
         gap: '50px',
       }}
     >
-      <Profile
+      {/* <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      />
+      /> */}
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
