3e8d0e5adcf69ebd1c527e3f8ab496c08e3f4044
Natasha <natkamonte1992@gmail.com>
2023-01-12 18:42:59 +0200

    change title in statistics components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,7 +29,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />
+      <Statistics title="Statistics" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </div>
